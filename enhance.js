f7a2e8207ae00a2eb1b7fe39f70c5497f7b6101b
Takashi Kaneda <takashi.kaneda@gmail.com>
2016-10-19 05:27:35 +0900

    node version

--- enhance.js
+++
@@ -1,8 +0,0 @@
-import CognitoIdentityServiceProvider from 'aws-sdk/clients/cognitoidentityserviceprovider';
-import * as enhancements from './src';
-
-export * from './src';
-
-Object.keys(enhancements).forEach(key => {
-  CognitoIdentityServiceProvider[key] = enhancements[key];
-});
