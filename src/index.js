e19720dd88e9d01e884ac49675dc86f09bfc059b
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2017-10-19 22:06:44 +0200

    Move msCrypto check to index.js

--- src/index.js
+++ src/index.js
@@ -25,3 +25,10 @@ export { default as CognitoUserAttribute } from './CognitoUserAttribute';
 export { default as CognitoUserPool } from './CognitoUserPool';
 export { default as CognitoUserSession } from './CognitoUserSession';
 export { default as DateHelper } from './DateHelper';
+
+// The version of crypto-browserify included by aws-sdk only
+// checks for window.crypto, not window.msCrypto as used by
+// IE 11 – so we set it explicitly here
+if (typeof window !== 'undefined' && !window.crypto && window.msCrypto) {
+  window.crypto = window.msCrypto;
+}
