d2c1baa90f6947cc10760bf7b02257349f93c170
marlon_trapp <marlon.trapp@hexagonagriculture.com>
2017-11-13 08:30:24 -0200

    New build after rebase

--- dist/amazon-cognito-identity.js
+++ dist/amazon-cognito-identity.js
@@ -76,7 +76,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	exports.__esModule = true;
 
-	var _src = __webpack_require__(16);
+	var _src = __webpack_require__(17);
 
 	Object.keys(_src).forEach(function (key) {
 	  if (key === "default" || key === "__esModule") return;
@@ -3718,6 +3718,110 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	exports.__esModule = true;
 
+	var _jsCookie = __webpack_require__(18);
+
+	var Cookies = _interopRequireWildcard(_jsCookie);
+
+	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	/** @class */
+	var CookieStorage = function () {
+
+	  /**
+	   * Constructs a new CookieStorage object
+	   * @param {object} data Creation options.
+	   * @param {string} data.domain Cookies domain (mandatory).
+	   * @param {string} data.path Cookies path (default: '/')
+	   * @param {integer} data.expires Cookie expiration (in days, default: 365)
+	   * @param {boolean} data.secure Cookie secure flag (default: true)
+	   */
+	  function CookieStorage(data) {
+	    _classCallCheck(this, CookieStorage);
+
+	    this.domain = data.domain;
+	    if (data.path) {
+	      this.path = data.path;
+	    } else {
+	      this.path = '/';
+	    }
+	    if (data.expire) {
+	      this.expires = data.expires;
+	    } else {
+	      this.expires = 365;
+	    }
+	    if (data.hasOwnProperty('secure')) {
+	      this.secure = data.secure;
+	    } else {
+	      this.secure = true;
+	    }
+	  }
+
+	  /**
+	   * This is used to set a specific item in storage
+	   * @param {string} key - the key for the item
+	   * @param {object} value - the value
+	   * @returns {string} value that was set
+	   */
+
+
+	  CookieStorage.prototype.setItem = function setItem(key, value) {
+	    Cookies.set(key, value, { path: this.path, expires: this.expires, domain: this.domain });
+	    return Cookies.get(key);
+	  };
+
+	  /**
+	   * This is used to get a specific key from storage
+	   * @param {string} key - the key for the item
+	   * This is used to clear the storage
+	   * @returns {string} the data item
+	   */
+
+
+	  CookieStorage.prototype.getItem = function getItem(key) {
+	    return Cookies.get(key);
+	  };
+
+	  /**
+	   * This is used to remove an item from storage
+	   * @param {string} key - the key being set
+	   * @returns {string} value - value that was deleted
+	   */
+
+
+	  CookieStorage.prototype.removeItem = function removeItem(key) {
+	    return Cookies.remove(key, { path: this.path, domain: this.domain, secure: this.secure });
+	  };
+
+	  /**
+	   * This is used to clear the storage
+	   * @returns {string} nothing
+	   */
+
+
+	  CookieStorage.prototype.clear = function clear() {
+	    var cookies = Cookies.get();
+	    var index;
+	    for (index = 0; index < cookies.length; ++index) {
+	      Cookies.remove(cookies[index]);
+	    }
+	    return {};
+	  };
+
+	  return CookieStorage;
+	}();
+
+	exports.default = CookieStorage;
+
+/***/ }),
+/* 17 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	exports.__esModule = true;
+
 	var _AuthenticationDetails = __webpack_require__(14);
 
 	Object.defineProperty(exports, 'AuthenticationDetails', {
@@ -3799,6 +3903,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }
 	});
 
+	var _CookieStorage = __webpack_require__(16);
+
+	Object.defineProperty(exports, 'CookieStorage', {
+	  enumerable: true,
+	  get: function get() {
+	    return _interopRequireDefault(_CookieStorage).default;
+	  }
+	});
+
 	var _DateHelper = __webpack_require__(11);
 
 	Object.defineProperty(exports, 'DateHelper', {
@@ -3816,6 +3929,177 @@ return /******/ (function(modules) { // webpackBootstrap
 	if (typeof window !== 'undefined' && !window.crypto && window.msCrypto) {
 	  window.crypto = window.msCrypto;
 	}
+
+/***/ }),
+/* 18 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
+	 * JavaScript Cookie v2.1.4
+	 * https://github.com/js-cookie/js-cookie
+	 *
+	 * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
+	 * Released under the MIT license
+	 */
+	;(function (factory) {
+		var registeredInModuleLoader = false;
+		if (true) {
+			!(__WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.call(exports, __webpack_require__, exports, module)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+			registeredInModuleLoader = true;
+		}
+		if (true) {
+			module.exports = factory();
+			registeredInModuleLoader = true;
+		}
+		if (!registeredInModuleLoader) {
+			var OldCookies = window.Cookies;
+			var api = window.Cookies = factory();
+			api.noConflict = function () {
+				window.Cookies = OldCookies;
+				return api;
+			};
+		}
+	}(function () {
+		function extend () {
+			var i = 0;
+			var result = {};
+			for (; i < arguments.length; i++) {
+				var attributes = arguments[ i ];
+				for (var key in attributes) {
+					result[key] = attributes[key];
+				}
+			}
+			return result;
+		}
+
+		function init (converter) {
+			function api (key, value, attributes) {
+				var result;
+				if (typeof document === 'undefined') {
+					return;
+				}
+
+				// Write
+
+				if (arguments.length > 1) {
+					attributes = extend({
+						path: '/'
+					}, api.defaults, attributes);
+
+					if (typeof attributes.expires === 'number') {
+						var expires = new Date();
+						expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+						attributes.expires = expires;
+					}
+
+					// We're using "expires" because "max-age" is not supported by IE
+					attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
+
+					try {
+						result = JSON.stringify(value);
+						if (/^[\{\[]/.test(result)) {
+							value = result;
+						}
+					} catch (e) {}
+
+					if (!converter.write) {
+						value = encodeURIComponent(String(value))
+							.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+					} else {
+						value = converter.write(value, key);
+					}
+
+					key = encodeURIComponent(String(key));
+					key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+					key = key.replace(/[\(\)]/g, escape);
+
+					var stringifiedAttributes = '';
+
+					for (var attributeName in attributes) {
+						if (!attributes[attributeName]) {
+							continue;
+						}
+						stringifiedAttributes += '; ' + attributeName;
+						if (attributes[attributeName] === true) {
+							continue;
+						}
+						stringifiedAttributes += '=' + attributes[attributeName];
+					}
+					return (document.cookie = key + '=' + value + stringifiedAttributes);
+				}
+
+				// Read
+
+				if (!key) {
+					result = {};
+				}
+
+				// To prevent the for loop in the first place assign an empty array
+				// in case there are no cookies at all. Also prevents odd result when
+				// calling "get()"
+				var cookies = document.cookie ? document.cookie.split('; ') : [];
+				var rdecode = /(%[0-9A-Z]{2})+/g;
+				var i = 0;
+
+				for (; i < cookies.length; i++) {
+					var parts = cookies[i].split('=');
+					var cookie = parts.slice(1).join('=');
+
+					if (cookie.charAt(0) === '"') {
+						cookie = cookie.slice(1, -1);
+					}
+
+					try {
+						var name = parts[0].replace(rdecode, decodeURIComponent);
+						cookie = converter.read ?
+							converter.read(cookie, name) : converter(cookie, name) ||
+							cookie.replace(rdecode, decodeURIComponent);
+
+						if (this.json) {
+							try {
+								cookie = JSON.parse(cookie);
+							} catch (e) {}
+						}
+
+						if (key === name) {
+							result = cookie;
+							break;
+						}
+
+						if (!key) {
+							result[name] = cookie;
+						}
+					} catch (e) {}
+				}
+
+				return result;
+			}
+
+			api.set = api;
+			api.get = function (key) {
+				return api.call(api, key);
+			};
+			api.getJSON = function () {
+				return api.apply({
+					json: true
+				}, [].slice.call(arguments));
+			};
+			api.defaults = {};
+
+			api.remove = function (key, attributes) {
+				api(key, '', extend(attributes, {
+					expires: -1
+				}));
+			};
+
+			api.withConverter = init;
+
+			return api;
+		}
+
+		return init(function () {});
+	}));
+
 
 /***/ })
 /******/ ])
